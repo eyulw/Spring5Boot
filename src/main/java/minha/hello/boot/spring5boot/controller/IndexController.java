46b8140134c2bec28c69371f98cb48e05e4c5ad1
eyulw <alsgk6989@gmail.com>
2023-07-05 12:18:04 +0900

    페이지 매핑 부분 추가

--- src/main/java/minha/hello/boot/spring5boot/controller/IndexController.java
+++ src/main/java/minha/hello/boot/spring5boot/controller/IndexController.java
@@ -1,13 +1,30 @@
 package minha.hello.boot.spring5boot.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
+    Logger logger = LogManager.getLogger(IndexController.class);
     @GetMapping("/")
     public String index(){
-
+        logger.info("index 호출");
         return "index";
     }
+
+    @GetMapping("/intro")
+    public String intro(){
+        logger.info("intro 호출");
+        return "intro";
+    }
+
+    @GetMapping("/admin")
+    public String admin(){
+        logger.info("admin 호출");
+        return "admin";
+    }
+
 }
