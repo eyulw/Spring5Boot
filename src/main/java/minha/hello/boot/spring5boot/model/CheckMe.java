698dea1f7f8270a7ef84e328c7521939502c490a
eyulw <alsgk6989@gmail.com>
2023-07-06 15:50:34 +0900

    회원가입 : 실명확인 정보용 클래스

---
+++ src/main/java/minha/hello/boot/spring5boot/model/CheckMe.java
@@ -0,0 +1,14 @@
+package minha.hello.boot.spring5boot.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class CheckMe {
+    private String name2;
+    private String jumin1;
+    private String jumin2;
+}
