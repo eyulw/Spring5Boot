3cb310cf7c84f2b16e7c69dc3ade37fba2d99b6c
eyulw <alsgk6989@gmail.com>
2023-07-07 11:59:29 +0900

    MemberMapper 매핑 namespace 변경 / MemberDAOImpl Mapper 호출방식 변경

--- src/main/java/minha/hello/boot/spring5boot/dao/MemberDAOImpl.java
+++ src/main/java/minha/hello/boot/spring5boot/dao/MemberDAOImpl.java
@@ -1,6 +1,8 @@
 package minha.hello.boot.spring5boot.dao;
 
+import lombok.RequiredArgsConstructor;
 import minha.hello.boot.spring5boot.model.Member;
+import minha.hello.boot.spring5boot.mybatis.MemberMapper;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -8,15 +10,23 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository("mdao")
+@RequiredArgsConstructor
 public class MemberDAOImpl implements MemberDAO{
+
+//    mybatis를 사용하기 위해 필요한 객체 DI
+//    단, 생성자 주입 방식 사용! (@RequiredArgsConstructor)
+    @Autowired
+    final MemberMapper memberMapper;
 
     @Autowired
     private SqlSession sqlSession;
+
     @Override
     public int insertMember(Member m) {
         //insert(insert관련맵핑,매개변수)
-        //네임스페이스.(id)
-        return sqlSession.insert("member.insertMember",m);
+        //sqlSession.insert(~,m)로 사용하는 방식보다는 편리
+//        return sqlSession.insert("MemberMapper.insertMember",m);
+        return memberMapper.insertMember(m);
     }
 
     @Override
