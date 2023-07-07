bceb23d0d6605e5ea723db46891c101db83d8cf5
eyulw <alsgk6989@gmail.com>
2023-07-07 12:00:19 +0900

    MemberMapper 단위테스트 추가

---
+++ src/test/java/minha/hello/boot/spring5boot/MemberMapperUnitTest.java
@@ -0,0 +1,29 @@
+package minha.hello.boot.spring5boot;
+
+import minha.hello.boot.spring5boot.model.Member;
+import minha.hello.boot.spring5boot.mybatis.MemberMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@MybatisTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class MemberMapperUnitTest {
+
+    @Autowired
+    private MemberMapper memberMapper;
+
+    @Test
+    @DisplayName("MemberMapper insert Test")    /*테스트명 지정가능*/
+    void insertMember(){
+        Member m = new Member(null,"","","","","","","","",null);
+        int result = memberMapper.insertMember(m);
+        System.out.println(result);
+        assertEquals(result,1);
+    }
+
+}
