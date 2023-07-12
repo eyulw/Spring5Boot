2a4f7ed288bcd14bd9fda01002b3199b515affd4
eyulw <alsgk6989@gmail.com>
2023-07-12 10:12:59 +0900

    게시판 : board mapper 생성

---
+++ src/main/java/minha/hello/boot/spring5boot/mybatis/BoardMapper.java
@@ -0,0 +1,14 @@
+package minha.hello.boot.spring5boot.mybatis;
+
+import minha.hello.boot.spring5boot.model.Board;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface BoardMapper {
+    int insertBoard(Board b);
+    List<Board> selectBoard(int stnum);
+    Board selectOneBoard(String bno);
+    int countPages();
+}
