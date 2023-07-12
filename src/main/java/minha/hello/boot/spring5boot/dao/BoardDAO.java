c42ac6ce5b04b389d6e9402417fd9d2a888c3b67
eyulw <alsgk6989@gmail.com>
2023-07-12 10:14:33 +0900

    게시판 : BoardDAO 구현

---
+++ src/main/java/minha/hello/boot/spring5boot/dao/BoardDAO.java
@@ -0,0 +1,13 @@
+package minha.hello.boot.spring5boot.dao;
+
+import minha.hello.boot.spring5boot.model.Board;
+
+import java.util.List;
+
+public interface BoardDAO {
+    int insertBoard(Board b);
+    List<Board> selectBoard(int stnum);
+    Board selectOneBoard(String bno);
+
+    int countPages();
+}
---
+++ src/main/java/minha/hello/boot/spring5boot/dao/BoardDAOImpl.java
@@ -0,0 +1,36 @@
+package minha.hello.boot.spring5boot.dao;
+
+import lombok.RequiredArgsConstructor;
+import minha.hello.boot.spring5boot.model.Board;
+import minha.hello.boot.spring5boot.mybatis.BoardMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository("bdao")
+@RequiredArgsConstructor
+public class BoardDAOImpl implements BoardDAO{
+
+    /* @Autowired 없이 DI 구현 */
+    final BoardMapper boardMapper;
+    @Override
+    public int insertBoard(Board b) {
+        return boardMapper.insertBoard(b);
+    }
+
+    @Override
+    public List<Board> selectBoard(int stnum) {
+        return boardMapper.selectBoard(stnum);
+    }
+
+    @Override
+    public Board selectOneBoard(String bno) {
+        return boardMapper.selectOneBoard(bno);
+    }
+
+    @Override
+    public int countPages() {
+        return boardMapper.countPages();
+    }
+}
