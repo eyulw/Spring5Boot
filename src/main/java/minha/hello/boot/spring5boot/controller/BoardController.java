aa6307643d38d61d11c08b5ff4f95edc5483b80e
eyulw <alsgk6989@gmail.com>
2023-07-12 15:41:44 +0900

    게시판 : BoardController view 구현

--- src/main/java/minha/hello/boot/spring5boot/controller/BoardController.java
+++ src/main/java/minha/hello/boot/spring5boot/controller/BoardController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/board")
@@ -16,12 +17,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class BoardController {
     Logger logger = LogManager.getLogger(BoardController.class);
     final BoardService bsrv;
-    @GetMapping("/list")
-    public String list(Model m, Integer cpg){
+    @GetMapping("/list/{cpg}")
+    public String list(Model m, @PathVariable Integer cpg){
         m.addAttribute("bds",bsrv.readBoard(cpg));
         m.addAttribute("psnum",10*((cpg-1)/10)+1);
         m.addAttribute("allpg",bsrv.getCountPages());
+        m.addAttribute("cpg",cpg);
         logger.info("board/list 호출");
         return "board/list";
+    }
+
+    @GetMapping("/view/{bno}")
+    public String view(Model m, @PathVariable String bno){
+        m.addAttribute("bd",bsrv.readOneBoard(bno));
+        logger.info("board/view 호출");
+        return "board/view";
     }
 }
