00e1f1ccca57879235fc8abf4bd72f70a11475ea
eyulw <alsgk6989@gmail.com>
2023-07-12 18:06:48 +0900

    게시판 : BoardController write 단위테스트

--- src/test/java/minha/hello/boot/spring5boot/board/BoardControllerUnitTest.java
+++ src/test/java/minha/hello/boot/spring5boot/board/BoardControllerUnitTest.java
@@ -8,8 +8,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -37,6 +39,19 @@ public class BoardControllerUnitTest {
     public void view() throws Exception {
         mvc.perform(get("/board/view/1"))
                         .andExpect(status().isOk())
+                        .andDo(print());
+    }
+
+    @Test
+    @DisplayName("BoardController write Test")
+    @Transactional
+    void write() throws Exception {
+        mvc.perform(post("/board/write")
+                        .param("userid","abc123a")
+                        .param("title","a")
+                        .param("contents","a")
+                        .param("ipaddr","a"))
+                        .andExpect(status().is3xxRedirection())
                         .andDo(print());
     }
 }
