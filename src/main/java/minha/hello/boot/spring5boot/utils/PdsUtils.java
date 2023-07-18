936556017ef2e4d77771866f2d96ed53d2233172
eyulw <alsgk6989@gmail.com>
2023-07-18 11:05:24 +0900

    자료실 : 다운로드 - PdsUtils getHeader,getResource 구현

--- src/main/java/minha/hello/boot/spring5boot/utils/PdsUtils.java
+++ src/main/java/minha/hello/boot/spring5boot/utils/PdsUtils.java
@@ -4,11 +4,17 @@ import minha.hello.boot.spring5boot.model.PdsAttach;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.util.UriUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
@@ -29,7 +35,7 @@ public class PdsUtils {
         int pos=pa.getFname().lastIndexOf(".")+1;
         pa.setFtype(pa.getFname().substring(pos));
 
-//        업로드할 파일 정보 알아내기 - 파일 크기
+//        업로드할 파일 정보 알아내기 - 파일 크기 kb단위로 끊어서저장
         pa.setFsize(attach.getSize()/1024+"");
 
 //        첨부파일을 지정한 위치에 저장
@@ -47,5 +53,30 @@ public class PdsUtils {
         String uuid= LocalDate.now()+""+ LocalTime.now();
         uuid = uuid.replace("-","").replace(":","").replace(".","");
         return uuid;
+    }
+
+    public HttpHeaders getHeader(String fname) {
+        HttpHeaders header=new HttpHeaders();
+        /*다운로드 할 파일의 이름을 인코딩함 - 한글처리*/
+        fname= UriUtils.encode(fname, StandardCharsets.UTF_8);
+        try {
+            /*다운로드 할 파일의 유형을 헤더에 저장*/
+            header.add("Content-Type", Files.probeContentType(Paths.get(saveDir+fname)));
+            header.add("Content-Disposition","attachment; filename="+fname);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return header;
+    }
+
+    public UrlResource getResource(String fname) {
+        UrlResource resource = null;
+        fname = UriUtils.encode(fname,StandardCharsets.UTF_8);
+        try {
+            resource=new UrlResource("file:"+saveDir+fname);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return resource;
     }
 }
