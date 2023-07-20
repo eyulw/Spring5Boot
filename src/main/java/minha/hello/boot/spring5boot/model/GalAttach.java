63a1446d976c21d4e246fb313edcbc21126d574f
minha <alsgk6989@gmail.com>
2023-07-20 11:17:56 +0900

    갤러리 : Gallery / GalAttach 클래스 추가

---
+++ src/main/java/minha/hello/boot/spring5boot/model/GalAttach.java
@@ -0,0 +1,15 @@
+package minha.hello.boot.spring5boot.model;
+
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class GalAttach {
+    private String gano;
+    private String gno;
+    private String fname;
+    private String fsize;
+}
---
+++ src/main/java/minha/hello/boot/spring5boot/model/Gallery.java
@@ -0,0 +1,21 @@
+package minha.hello.boot.spring5boot.model;
+
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Gallery {
+    private String gno;
+    private String title;
+    private String userid;
+    private String regdate;
+    private String thumbs;
+    private String views;
+    private String contents;
+    private String ipaddr;
+
+    private GalAttach ga;
+}
