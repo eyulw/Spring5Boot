18115f5489c149310762f6e70207baed5596bbe1
minha <alsgk6989@gmail.com>
2023-07-21 10:52:12 +0900

    갤러리 : gallery / galattach : 테이블,제약조건 생성, fname,fsize 컬럼사이즈 증가

--- spring5boot.sql
+++ spring5boot.sql
@@ -105,3 +105,35 @@ insert into pdscomments(userid,comments,ref,pno) values ('def123','대댓글1','
 insert into pdscomments(userid,comments,ref,pno) values ('abc123a','댓글4','5','7');
 
 select * from pdscomments where pno=7 order by ref;
+
+
+/*gallery*/
+create table gallery(
+    gno         int             auto_increment,
+    title       varchar(100)    not null ,
+    userid      varchar(18)     not null ,
+    regdate     datetime        default current_timestamp,
+    thumbs      int             default 0,
+    views       int             default 0,
+    contents    text            not null ,
+    ipaddr      varchar(15)     not null,
+    primary key (gno)
+);
+
+/*galattach*/
+create table galattach(
+  gano          int             auto_increment,
+  gno           int             not null ,  /*게시글 번호*/
+  fname         varchar(1024)   not null ,  /*uuid 포함*/
+  fsize         varchar(256)   not null ,
+  primary key (gano)
+);
+
+alter table gallery add constraint fkguid foreign key (userid) references member2(userid);
+
+alter table galattach add constraint fkgno foreign key (gno) references gallery(gno);
+
+/*
+        gno			fname		         fsize
+        1		abc123.png;87xyz.jpg	  100; 456
+*/
